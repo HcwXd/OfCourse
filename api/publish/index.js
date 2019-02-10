d4614a46eea285b16bd7843c69c0d0184d64aed4
HcwXd <e04dl47878@gmail.com>
2019-02-10 11:26:37 -0800

    Add all

--- api/publish/index.js
+++ api/publish/index.js
@@ -5,11 +5,8 @@ const router = express.Router();
 const reviewTable = require('../../database/reviewTable');
 const courseTable = require('../../database/courseTable');
 
-
 router.get('/:userId/', function(req, res) {
   res.render('publish');
 });
-
-
 
 module.exports = router;
--- public/publish/js/index.js
+++ public/publish/js/index.js
@@ -1,8 +1,43 @@
+const categoryMap = ['WebDev', 'MachineLearning', 'Algorithm'];
+let courseIdState;
+
 document.querySelectorAll('.star').forEach((el) => el.addEventListener('click', lightenStar));
 document.querySelector('.url_input_btn').addEventListener('click', checkUrlExist);
 
 function checkUrlExist() {
-  document.querySelector('.course_url_input').value;
+  const checkCourseExistUrl = '/courses?url=' + document.querySelector('.course_url_input').value;
+  fetch(checkCourseExistUrl, {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => {
+      if (!response) {
+        courseIdState = -1;
+      } else {
+        console.log(response);
+        const {
+          courseName,
+          courseId,
+          categoryId,
+          courseProvider,
+          des,
+          duration,
+          language,
+          level,
+          url,
+        } = response;
+        courseIdState = courseId;
+        document.querySelector('.course_name_input').value = courseName;
+        document.querySelector('.course_category_input').value = categoryMap[categoryId];
+        document.querySelector('.course_language_input').value = 'En';
+        document.querySelector('.course_level_input').value = 'Basic';
+      }
+    });
 }
 
 function lightenStar() {
@@ -20,6 +55,7 @@ function lightenStar() {
 
 document.querySelector('.submit_btn').addEventListener('click', () => {
   let payload = {};
+  let courseInfo = {};
   payload.url = document.querySelector('.course_url_input').value;
   payload.name = document.querySelector('.course_name_input').value;
   payload.category = document.querySelector('.course_category_input').value;
@@ -29,5 +65,19 @@ document.querySelector('.submit_btn').addEventListener('click', () => {
   payload.text = document.querySelector('.review').value;
   payload.date = document.querySelector('.course_name_input').value;
   console.log(payload);
-  // reviewId userId courseId collectionId score text date
+
+  const postPublishUrl = '/publish/1';
+  fetch(postPublishUrl, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => {
+      window.location = '/courses/' + response.courseId;
+    });
 });
