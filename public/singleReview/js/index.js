240969cd2ffa5336e9ab4d23c73836fe7f82d788
HcwXd <e04dl47878@gmail.com>
2019-02-10 01:34:24 -0800

    singleReview get api response

--- public/singleReview/js/index.js
+++ public/singleReview/js/index.js
@@ -9,11 +9,11 @@ fetch(window.location.pathname + '/courseInfo', {
   .catch((error) => console.error('Error:', error))
   .then((response) => {
     console.log(response);
-
-    document.querySelector('.title_wrap').innerHTML = '';
-    document.querySelector('.course_average_score').innerHTML = '';
-    document.querySelector('.course_reviews_count').innerHTML = '';
-    document.querySelector('.course_provider').innerHTML = '';
+    const { courseAvgScore, courseName, courseReviewCount, courseProvider } = response;
+    document.querySelector('.title_wrap').innerHTML = courseName;
+    document.querySelector('.course_average_score').innerHTML = courseAvgScore + ' ☆';
+    document.querySelector('.course_reviews_count').innerHTML = courseReviewCount + ' Reviews';
+    document.querySelector('.course_provider').innerHTML = courseProvider || '';
   });
 
 fetch(window.location.pathname + '/reviewInfo', {
@@ -27,11 +27,12 @@ fetch(window.location.pathname + '/reviewInfo', {
   .catch((error) => console.error('Error:', error))
   .then((response) => {
     console.log(response);
+    const { userAvatar, userName, userReviewCount, reviewText, reviewScore, reviewDate } = response;
 
-    document.querySelector('.user_avatar').src = '';
-    document.querySelector('.user_name').innerHTML = '';
-    document.querySelector('.user_reviews_count').innerHTML = '';
-    document.querySelector('.review_stats').innerHTML = '';
-    document.querySelector('.review_date').innerHTML = '';
-    document.querySelector('.review_text').innerHTML = '';
+    document.querySelector('.user_avatar').src = userAvatar;
+    document.querySelector('.user_name').innerHTML = userName;
+    document.querySelector('.user_reviews_count').innerHTML = userReviewCount;
+    document.querySelector('.review_score').innerHTML = reviewScore;
+    document.querySelector('.review_date').innerHTML = reviewDate;
+    document.querySelector('.review_text').innerHTML = reviewText;
   });
