823bbe45f27fbc8ad964e5c5d584c5caad263af1
tall15421542 <b04703001@ntu.edu.tw>
2019-02-10 11:24:26 -0800

    send courseId when publish finishing

--- api/publish/index.js
+++ api/publish/index.js
@@ -16,6 +16,7 @@ router.post('/newReview', async (req, res) => {
     req.reviewInfo.courseId = result.insertId;
   }
   const resutl = await reviewInfo.createReview(body.reviewInfo);
+  res.status(200).send(req.reviewInfo.courseId);
 })
 
 module.exports = router;
