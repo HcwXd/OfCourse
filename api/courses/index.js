cdd217cf5f53f0925814cc343522c694e24871a1
HcwXd <e04dl47878@gmail.com>
2019-02-09 23:31:28 -0800

    Revise singleReview url

--- api/courses/index.js
+++ api/courses/index.js
@@ -3,9 +3,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:courseId/reviews/:reviewId', function(req, res) {
-  res.render('singleReviewDetail');
+router.get('/:courseId/users/:userId/reviews/:reviewId', function(req, res) {
+  res.render('singleReview');
 });
-router.get('/:courseId/reviews/:reviewId/info', require('./controller/getReviewsInfo'));
+
+router.get('/:courseId', function(req, res) {
+  res.render('singleCourse');
+});
+
+router.get(
+  '/:courseId/users/:userId/reviews/:reviewId/info',
+  require('./controller/getReviewsInfo')
+);
 
 module.exports = router;
